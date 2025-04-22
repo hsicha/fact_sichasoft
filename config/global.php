20d0bc4ff7d7c2aa83acf7a62d0fd12d25a04cec
sicha <hsicharomani@gmail.com>
2025-04-22 12:22:14 -0500

    se sube  el proyecto

---
+++ config/global.php
@@ -0,0 +1,18 @@
+<?php 
+define('URL','https://www.localhost/sichasoft/');
+//Ip de la pc servidor de base de datos
+define("DB_HOST","demo.sichasoft.com");
+
+//Nombre de la base de datos
+define("DB_NAME", "demosi_data");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "demosi_sicha");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", "demo@2023");
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
+
+?>
